c1e7ce9ffddfb7290e927c7410c90de44804f679
Vinod Badgujar <vbadgujar1999@gmail.com>
2020-05-23 09:29:05 +0530

    tt

--- MLOps_5.py
+++ MLOps_5.py
@@ -6,6 +6,7 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.metrics import accuracy_score
 
 # In[2]:
 #dataset = pd.read_csv(url, error_bad_lines=False
@@ -64,7 +65,7 @@ y_prediction = mind.predict(x_test)
 
 
 
-from sklearn import metrics
+
 
 
 # In[31]:
@@ -72,9 +73,8 @@ from sklearn import metrics
 
 # In[32]:
 
-metrics.mean_squared_error(y_test,y_prediction)
-acc= np.mean((abs(y_test)-abs(y_prediction))/2)
 
+acc=accuracy_score(y_test,y_prediction)
 
 
 # In[35]:
