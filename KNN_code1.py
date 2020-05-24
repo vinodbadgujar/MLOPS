aa787e40bd8826f62aaf03e6c534e93d042c82d4
Vinod Badgujar <vbadgujar1999@gmail.com>
2020-05-24 10:39:33 +0530

    knn code

---
+++ KNN_code1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+import pandas as pd
+dataset = pd.read_csv("/programs/Social_Network_Ads.csv")
+X = dataset[['Age', 'EstimatedSalary' ] ] 
+y = dataset['Purchased']
+X = X.values
+y = y.values
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+from sklearn.neighbors import KNeighborsClassifier
+model = KNeighborsClassifier(n_neighbors=4)
+model.fit(X_train, y_train)
+model.predict([[ 61, 200000 ]] )
+y_pred = model.predict(X_test)
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_test, y_pred)
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+ptint("Accuracy of the KNN model is :", accuracy)
+
+
+
+
+
+
+
+
+
+
+
+
